174232ebcc358f3b08dd07ecfe0cbd4897584c32
JaguarBurgerMike <michalpieta27@gmail.com>
2026-06-14 19:54:40 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import sync_playwright
+import re
 
 URL = "https://bip.malopolska.pl/umbochnia,m,276530,nabor-na-stanowiska-urzednicze-konkursy.html"
 
@@ -10,8 +11,19 @@ with sync_playwright() as p:
 
     page.goto(URL, wait_until="networkidle")
 
-    print(page.title())
-
-    print(page.locator("body").inner_text()[:5000])
+    text = page.locator("body").inner_text()
 
     browser.close()
+
+matches = re.findall(
+    r"Przejdź do:\s*(.*?)\s+(Ogłoszony|Zakończony)\s+(\d{4}-\d{2}-\d{2})",
+    text,
+    re.DOTALL
+)
+
+print("\nZNALEZIONE OGŁOSZENIA:\n")
+
+for title, status, date in matches:
+    print(f"[{date}] [{status}]")
+    print(title.strip())
+    print("-" * 50)
