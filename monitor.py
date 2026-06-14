e63f781af35f41ff8283d6df226f435be64da420
JaguarBurgerMike <michalpieta27@gmail.com>
2026-06-14 19:49:06 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,43 +1,12 @@
 import requests
-from bs4 import BeautifulSoup
-import json
-import os
 
-print("=== NOWA WERSJA SKRYPTU ===")
+url = "https://bip.malopolska.pl/umbochnia,m,276530,nabor-na-stanowiska-urzednicze-konkursy.html"
 
-DB_FILE = "database.json"
+r = requests.get(
+    url,
+    headers={"User-Agent": "Mozilla/5.0"}
+)
 
-with open("sites.json", "r", encoding="utf-8") as f:
-    sites = json.load(f)
-
-if os.path.exists(DB_FILE):
-    with open(DB_FILE, "r", encoding="utf-8") as f:
-        database = json.load(f)
-else:
-    database = {}
-
-for name, url in sites.items():
-
-    print(f"\nSprawdzam: {name}")
-
-    r = requests.get(
-        url,
-        headers={"User-Agent": "Mozilla/5.0"},
-        timeout=20
-    )
-
-    soup = BeautifulSoup(r.text, "html.parser")
-
-    current = {}
-
-    for a in soup.find_all("a", href=True):
-
-        href = a["href"]
-        title = a.get_text(" ", strip=True)
-
-        print("LINK:", title, "|", href)
-
-    print("KONIEC TESTU")
-
-with open(DB_FILE, "w", encoding="utf-8") as f:
-    json.dump(database, f, ensure_ascii=False, indent=2)
+print("STATUS:", r.status_code)
+print()
+print(r.text[:5000])
